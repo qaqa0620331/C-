2aa419688a560316a4e572f45541e0588c99a289
qaqa0620331 <43431973+qaqa0620331@users.noreply.github.com>
2018-11-20 16:15:29 +0800

    Add files via upload

---
+++ 20181120/5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8"> 
+<title>菜鸟教程(runoob.com)</title> 
+</head>
+<body>
+
+<embed <iframe width="1280" height="720" src="https://www.youtube.com/embed/mAKsZ26SabQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></embed>
+<embed <iframe width="1280" height="720" src="https://www.youtube.com/embed/cBVGlBWQzuc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></embed>
+<video width="1920" height="1080" controls autoplay>
+    <source src="movie.mp4" type="video/mp4">
+    <object data="movie.mp4" width="1920" height="1080">
+    <embed width="1920" height="1080" src="movie.swf">
+  </object>
+</video>
+<img src="giphy.gif" alt="Pulpit rock" width="304" height="228">
+<img src="https://iplogger.org/1pKA87" alt="iplogger.org - IP Logging Service" border="0">
+<body ondragstart="return false" onselectstart="return false" onContextMenu="return false"></body>
+</body>
+</html>
